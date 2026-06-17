a9327f21856b258e24da36770552a0dc463b2247
MostafaSensei106 <wwwmostafagmer100'gmail.com>
2026-06-17 13:13:06 +0300

    Refactor response handling: update ErrorInfo structure and implement Responser methods for improved response management

--- Golang/internal/core/delivery/response.go
+++ Golang/internal/core/delivery/response.go
@@ -1,12 +1,14 @@
 package delivery
 
+import "net/http"
+
 type Context interface {
 	JSON(code int, data any)
 }
 
 type ErrorInfo struct {
-	ErrorCode string `json:"error_code"`
-	Message   string `json:"message"`
+	Code    string `json:"code"`
+	Message string `json:"message"`
 }
 
 type Meta struct {
@@ -19,4 +21,52 @@ type Meta struct {
 }
 
 type Response struct {
+	Success bool       `json:"success"`
+	Data    any        `json:"data,omitempty"`
+	Error   *ErrorInfo `json:"error,omitempty"`
+	Meta    *Meta      `json:"meta,omitempty"`
+}
+
+type Responser struct {
+	ctx        Context
+	statusCode int
+	response   Response
+}
+
+func NewResponser(ctx Context) *Responser {
+	return &Responser{
+		ctx:        ctx,
+		statusCode: http.StatusOK,
+		response: Response{
+			Success: true,
+		},
+	}
+}
+
+func (r *Responser) Status(code int) *Responser {
+	r.statusCode = code
+	return r
+}
+
+func (r *Responser) WithData(data any) *Responser {
+	r.response.Data = data
+	return r
+}
+
+func (r *Responser) WithMeta(meta *Meta) *Responser {
+	r.response.Meta = meta
+	return r
+}
+
+func (r *Responser) WithError(code string, message string) *Responser {
+	r.response.Success = false
+	r.response.Error = &ErrorInfo{
+		Code:    code,
+		Message: message,
+	}
+	return r
+}
+
+func (r *Responser) Send() {
+	r.ctx.JSON(r.statusCode, r.response)
 }
