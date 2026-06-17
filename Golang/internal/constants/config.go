81a8111e467395e8e29157b7e61bbcd778c97f65
MostafaSensei106 <wwwmostafagmer100'gmail.com>
2026-06-17 16:11:46 +0300

    Add constants for configuration and headers in the backend

---
+++ Golang/internal/constants/config.go
@@ -0,0 +1,8 @@
+package constants
+
+const (
+	MinTrustScore           = 50
+	MaxStreakMultiplier     = 2.0
+	StreakMultiplierPercent = 0.05
+	JwtField                = "user_id"
+)
---
+++ Golang/internal/constants/headers/headers.go
@@ -0,0 +1,14 @@
+package headers
+
+const (
+	CONTENT_TYPE                      = "Content-Type"
+	APPLICATION_JSON                  = "application/json"
+	APPLICATION_XML                   = "application/xml"
+	APPLICATION_OCTET_STREAM          = "application/octet-stream"
+	APPLICATION_FORM_URLENCODED       = "application/x-www-form-urlencoded"
+	APPLICATION_PDF                   = "application/pdf"
+	APPLICATION_GZIP                  = "application/gzip"
+	APPLICATION_X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded"
+	ACCEPT_LANGUAGE                   = "Accept-Language"
+	AUTHORIZATION                     = "Authorization"
+)
