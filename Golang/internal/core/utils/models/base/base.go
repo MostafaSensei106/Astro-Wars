e065dde943a3db6f837e261da7786c794cd8ac9a
MostafaSensei106 <wwwmostafagmer100'gmail.com>
2026-06-17 13:25:05 +0300

    Add BaseModel struct for GORM integration and update go.mod and go.sum for new dependencies

---
+++ Golang/internal/core/utils/models/base/base.go
@@ -0,0 +1,14 @@
+package base
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type BaseModel struct {
+	ID        string         `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+	CreatedAt time.Time      `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time      `json:"updated_at" gorm:"autoUpdateTime"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index" `
+}
